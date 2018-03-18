b950106259fd528bfda5affa08bd72a6fbd01ceb
jumasheff <jumasheff@gmail.com>
2018-03-18 10:59:57 +0600

    Add production settings

---
+++ mysite/settings_prod.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'simpledjango',
+        'USER': 'simpledjangouser',
+        'PASSWORD': '123simpledjango123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
